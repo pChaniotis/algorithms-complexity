0316c1b63671b7fa0ed78756c0708974f69074b6
root <root@LAPTOP-FIL0HS3D.localdomain>
2022-05-09 16:39:42 +0300

    total_weight_of_sides completed

--- maxcut_single.py
+++ maxcut_single.py
@@ -1,6 +1,5 @@
-import random
-
 import networkx as nx
+import matplotlib.pyplot as plt
 import random
 
 def create_weighted_graph(n, p, max_weight = 10, seed=None):
@@ -13,10 +12,31 @@ def create_weighted_graph(n, p, max_weight = 10, seed=None):
     return G
 
 def total_weight_of_sides(G, v, A, B):
-    total_weight = 0
-    # Complete the code
+    sum_A = 0
+    sum_B = 0
+    isA = False
 
-    return total_weight
+    edge_attributes = nx.get_edge_attributes(G,"weight")
+    for node in G.neighbors(v):
+        for a in A:
+            if (a == v):
+                isA = True;
+            if node == a:
+                sum_A += edge_attributes[(v,node)]
+        for b in B:
+            if node == b:
+                sum_B += edge_attributes[(v,node)]
+        
+                
+    if isA: 
+        sum_own_side = sum_A 
+        sum_other_side = sum_B
+    else:
+        sum_own_side = sum_B
+        sum_other_side = sum_A
+        
+    return sum_own_side, sum_other_side
+
 
 def local_search_maxcut(G, A, B, seed):
     # Random Number Generator
@@ -24,10 +44,23 @@ def local_search_maxcut(G, A, B, seed):
     # Use this random number generator if necessary.
     # For example rng.randint(0, 10) for a uniformly random integer r: 0 <= r <= 10
 
-    # Complete the code
+    random_node = rng.randint(0,len(G.nodes))
+    
+    for v in G.neighbors(random_node):
+        while True:    
+            is_A = True if v in A else False
+            this_side, other_side = total_weight_of_sides(G, v, A, B)
 
-
-    # Return the partition
+            if (this_side > other_side):
+                if(is_A):
+                    A.remove(v)
+                    B.append(v)
+                else:
+                    B.remove(v)
+                    A.append(v)
+            else:
+                break;
+    
     return A, B
 
 def check_cut_value(G, A, B, threshold):
@@ -38,12 +71,16 @@ def check_cut_value(G, A, B, threshold):
         return f'The cut_value is {cut_value} but should be at least {threshold}.'
 
 G = create_weighted_graph(12, 0.7, seed=12)
+nx.draw(G,with_labels=True);
+plt.savefig("A");
 
 # Create a random partition (partition_A, partition_B)
-# Complete the code
+nodes = list(G.nodes)
+partition_A = nodes[0:6]
+partition_B = nodes[6:12]
 
 # Run local search
-# A, B = local_search_maxcut(G, partition_A, partition_B, seed=123)
+A, B = local_search_maxcut(G, partition_A, partition_B, seed=123)
 
 # Check the value
-# print(check_cut_value(G, A, B, 199))
+print(check_cut_value(G, A, B, 199))
